c1317348c6b7d80e6d9e2ef54cc0436014a3dbf5
yassineh01 <132911589+yassineh01@users.noreply.github.com>
2023-05-11 14:24:01 +0200

    Add files via upload

---
+++ opdracht1.php
@@ -0,0 +1,22 @@
+<?php
+
+$nummer1 = 12;
+$nummer2 = 10;
+$naam = "Yassine";
+
+echo $nummer1 + $nummer2;
+
+echo $nummer1 * $nummer2;
+
+echo " Welkom " . $naam . "!";
+
+
+?>
+
+<?php
+
+$mijnNaam= "Carl";
+$jouwNaam= "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas "
+
+?>
